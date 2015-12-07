d82a7eeff15d6e13100137bac25b3896547009e8
Dave Willmer <dave.willmer@gmail.com>
2015-12-07 11:49:50 +0000

    Fix licence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-"""
-Copyright (c) 2015 Phosphor Contributors
-Distributed under the terms of the BSD 3-Clause License.
-The full license is in the file LICENSE, distributed with this software.
-"""
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
 import subprocess
 import sys
 
