75ad88da76feba7baedaf2e420f69f0b3d6d43f9
Dave Willmer <dave.willmer@gmail.com>
2015-12-07 11:39:43 +0000

    Initial working app

--- editor/index.ts
+++ editor/index.ts
@@ -2,7 +2,9 @@
 // Distributed under the terms of the Modified BSD License.
 'use strict';
 
-import CodeMirror = require('codemirror');
+import {
+  CodeMirror
+} from 'codemirror';
 
 import {
   Message
