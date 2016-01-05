cb282b05764ea7b352220c2dbf22f20b1ad68e11
Steven Silvester <steven.silvester@ieee.org>
2016-01-05 10:31:50 -0600

    Update to use phosphide plugins

--- index.js
+++ index.js
@@ -2,9 +2,19 @@
 // Distributed under the terms of the Modified BSD License.
 'use strict';
 
+var phosphide = require('phosphide');
+var di = require('phosphor-di');
 
-System.import('phosphor-plugins').then(lib => {
-  lib.registerPlugin('phosphide');
-  lib.registerPlugin('jupyter-js-filebrowser-plugin');
-  lib.registerPlugin('jupyter-js-editor-plugin');
-});
+
+function main() {
+   phosphide.loadPlugins(new di.Container(), [
+    require('phosphide/lib/appshell/plugin'),
+    require('jupyter-js-editor/lib/plugin'),
+    require('jupyter-js-terminal/lib/plugin'),
+    require('jupyter-js-filebrowser/lib/plugin')
+  ]).then(function() {
+    console.log('loading finished');
+  });
+}
+
+window.onload = main;
